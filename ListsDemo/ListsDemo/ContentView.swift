ef9ea77b6aba6012e53a0ee81b122a2ef013032e
Swapnil Magar <magarswapnil896@gmail.com>
2020-06-09 21:00:08 +0530

    Initial Project commit

---
+++ ListsDemo/ListsDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ListsDemo
+//
+//  Created by Swapnil  Magar on 09/06/20.
+//  Copyright © 2020 Swapnil Magar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
